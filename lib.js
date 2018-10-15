cf6bbd300c454e5c800d7891c36ef777405f31fa
DMRmejiar <36444462+DMRmejiar@users.noreply.github.com>
2018-10-14 20:58:11 -0500

    Update lib.js

--- lib.js
+++ lib.js
@@ -58,25 +58,26 @@ function create_operation(){
   switch (operator) {
     case 0:
       problem = number_one + " + " + number_two;
+      trap_num = number_one - number_two + 1;
       solution_number = number_one + number_two;
       break;
     case 1:
       problem = number_one + " - " + number_two;
+      trap_num = number_one + number_two + 1;
       solution_number = number_one - number_two;
       break;
     case 2:
       problem = number_one + " * " + number_two;
+      trap_num = number_one / number_two + 1;
       solution_number = number_one * number_two;
       break;
     case 3:
       problem = number_one + " / " + number_two;
+      trap_num = number_one * number_two + 1;
       solution_number = Math.floor(number_one / number_two);
       break;
     default:
   }
-  do{
-    trap_num = Math.floor(Math.random() * 251);
-  }while(trap_num == solution_number);
 }
 
 function init() {
